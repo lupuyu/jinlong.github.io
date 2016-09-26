b9b3ee1c805275a6ed1f01108247c9f6ddc60622
DESKTOP-4HSL9DI\lupuy <lu.puyu@qq.com>
2016-09-26 09:44:27 +0800

    09260944

--- application/index/controller/CountController.php
+++ application/index/controller/CountController.php
@@ -27,20 +27,70 @@ class CountController extends IndexController
     	$start_time=input('post.start');
     	$end_time=input('post.end');
     	$course=input('post.co');
+        $tongjia_info = array();
     	// var_dump($start_time);
     	// var_dump($end_time);
     	// var_dump($course);
     	// die();
         $result = Count::view('kechengbiao','id,status')
 		    ->view('student',['num','kclass','name','phone'],'kechengbiao.student_id=student.id')
-		    ->where('teacher_id',$teacherid)
-		    ->where('start_time','>=',$start_time)
-		    ->where('end_time','<=',$end_time)
-		    ->where('course_id',$course)
+		    ->where('teacher_id','2')
+		    ->where('start_time','>=','2016-09-01')
+		    ->where('end_time','<=','2016-09-30')
+		    ->where('course_id','2')
 		    ->order('id desc')
 		    ->select();
-		    $this->assign('result', $result);
-		dump($result);
+        //     while($row = mysql_fetch_array($result)){
+
+        //     //如果还没收录此人
+        //     if($tongjia_info[$row['num']][0] == '' ){
+        //         $tongjia_info[$row['num']][0] = $row['num'];
+        //         $tongjia_info[$row['num']][1] = $row['name'];
+        //         $tongjia_info[$row['num']][2] = $row['phone'];
+        //         $tongjia_info[$row['num']][3] = 0; //考勤次数
+        //         $tongjia_info[$row['num']][4] = 0; //正常次数
+        //         $tongjia_info[$row['num']][5] = 0; //旷课次数
+        //         $tongjia_info[$row['num']][6] = 0; //请假次数
+        //         $tongjia_info[$row['num']][7] = 0; //迟到次数
+        //         $tongjia_info[$row['num']][8] = 0; //早退次数
+        //         if($row['status'] == '2') $tongjia_info[$row['num']][5]++;
+        //         else if($row['status'] == '3') $tongjia_info[$row['num']][6]++;
+        //         else if($row['status'] == '4') $tongjia_info[$row['num']][7]++;
+        //         else if($row['status'] == '5') $tongjia_info[$row['num']][8]++;
+        //         else $tongjia_info[$row['num']][4]++;
+        //         $tongjia_info[$row['num']][3]++;
+        //     }else{//已经收录
+        //         if($row['status'] == '2') $tongjia_info[$row['num']][5]++;
+        //         else if($row['status'] == '3') $tongjia_info[$row['num']][6]++;
+        //         else if($row['status'] == '4') $tongjia_info[$row['num']][7]++;
+        //         else if($row['status'] == '5') $tongjia_info[$row['num']][8]++;
+        //         else $tongjia_info[$row['num']][4]++;
+        //         $tongjia_info[$row['num']][3]++;
+        //     }
+        // }
+        foreach ($result as $row ) {            
+            $tongjia_info[$row['num']][0] = $row['num'];
+            $tongjia_info[$row['num']][1] = $row['name'];
+            $tongjia_info[$row['num']][2] = $row['phone'];
+            $tongjia_info[$row['num']][3] = 0;  //考勤次数
+            $tongjia_info[$row['num']][4] = 0; //正常次数
+            $tongjia_info[$row['num']][5] = 0; //旷课次数
+            $tongjia_info[$row['num']][6] = 0; //请假次数
+            $tongjia_info[$row['num']][7] = 0; //迟到次数
+            $tongjia_info[$row['num']][8] = 0; //早退次数
+            if($row['status'] == '2') $tongjia_info[$row['num']][5]++;
+            else if($row['status'] == '3') $tongjia_info[$row['num']][6]++;
+            else if($row['status'] == '4') $tongjia_info[$row['num']][7]++;
+            else if($row['status'] == '5') $tongjia_info[$row['num']][8]++;
+            else $tongjia_info[$row['num']][4]++;
+            $tongjia_info[$row['num']][3]++;
+            
+        }
+		    dump($tongjia_info);
+            die();
+            $this->assign('tongjia_info', $tongjia_info);
+            return $this->fetch();
+
     }
 
 }
