0b2b4edf7cf89d65c67b007ed02149fe56cfa688
DESKTOP-JKA0P56\lupuyu <lu.puyu@qq.com>
2016-10-01 18:58:07 +0800

    no message

--- application/index/controller/CalendarController.php
+++ application/index/controller/CalendarController.php
@@ -12,6 +12,12 @@ class CalendarController  extends IndexController{
 		return view();
 	}
 
+	// 增删改处理
+	public function calendarDo(){
+		halt(input(''));
+		
+	}
+
 	public function json()
 	{
 		return view();
